62306bdcb38d9358f00ae53c44685c44d2ac6054
zwang253 <zwang253@mgmt-4.hkust-gz.edu.cn>
2023-11-10 00:07:01 +0800

    fix rendering

--- projects/mmdet3d_plugin/occformer/detectors/moeoccupancyscale_test.py
+++ projects/mmdet3d_plugin/occformer/detectors/moeoccupancyscale_test.py
@@ -407,10 +407,10 @@ class MoEOccupancyScale_Test(BEVDepth):
             # rgb_map = torch.sigmoid(rgb.sum(dim=-2)) 
             
             
-            rgb = torch.sigmoid(self.rgb_head(pts_feature)) # [H, W, D, 3]
-            # rgb[~mask] *= 0
+            rgb = self.rgb_head(pts_feature)
+            rgb[~mask] = -1e5
+            rgb = torch.sigmoid(rgb) # [H, W, D, 3]
             sigma = self.sigma_head(pts_feature).squeeze(-1) # [H, W, D]
-            # sigma[~mask] *= 0
             sigma = F.relu(sigma)
             
             pts = pts.float()
